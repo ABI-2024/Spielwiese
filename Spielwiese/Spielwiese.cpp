2bdd2e60f15cd2fbafcfc12b916d55d38eb05798
Aeneas Schwaiger <aeneas@schwaiger74.de>
2022-10-11 21:08:42 +0200

    StressFunktion eingefüht

--- Spielwiese/Spielwiese.cpp
+++ Spielwiese/Spielwiese.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+string funktion();
+
 int main()
 {
    cout << "Hier ist eine Spielwiese. Hier könnt ihr alles mal Testen.";
+   cout << funktion << endl;
+}
+
+string funktion() {
+	cout << "Erstellt eine Fnktion oder Klasse und versucht diese ohne Konflikte hier einzu fügen.";
 }
